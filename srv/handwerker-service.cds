1c9abbd31564876356ffa7d33c10f2a7a6028f97
Benedikt Hölker <benedikt.hoelker@iot-online.de>
2023-03-19 23:25:54 +0100

    chore: configure auth0

--- srv/handwerker-service.cds
+++ srv/handwerker-service.cds
@@ -1,8 +1,20 @@
 using {handwerker as my} from '../db/schema';
 
+@(requires: 'authenticated-user')
 service HandwerkerService {
     entity BusinessPartners as projection on my.BusinessPartners;
     entity Equipments       as projection on my.Equipments;
     entity Orders           as projection on my.Orders;
-    entity OrderItems       as projection on my.OrderItems;
+
+    @(restrict: [
+        {
+            grant: 'READ',
+            to   : 'read:orderitems'
+        },
+        {
+            grant: 'WRITE',
+            to   : 'write:orderitems'
+        },
+    ])
+    entity OrderItems       as projection on my.OrderItems
 }
--- srv/server.js
+++ srv/server.js
@@ -1,4 +1,25 @@
-const cds = require("@sap/cds");
-const cov2ap = require("@sap/cds-odata-v2-adapter-proxy");
-cds.on("bootstrap", (app) => app.use(cov2ap()));
+const cov2ap = require('@sap/cds-odata-v2-adapter-proxy');
+const cds = require('@sap/cds');
+const express = require('express');
+const { auth, requiresAuth } = require('express-openid-connect');
+
+const config = {
+  authRequired: false, // deactivate auth for all routes
+  auth0Logout: true, // logout from IdP
+  authorizationParams: {
+    // required to retrieve JWT including permissions (our roles)
+    response_type: 'code',
+    scope: 'openid',
+    audience: 'https://handwerker-api.com'
+  }
+};
+
+cds.on('bootstrap', (app) => {
+  // initialize openid-connect with auth0 configuration
+  app.use(auth(config));
+  app.use(cov2ap());
+  app.use('/app', requiresAuth(), express.static(__dirname + '/../app'));
+  app.use('/v2', requiresAuth());
+});
+
 module.exports = cds.server;
