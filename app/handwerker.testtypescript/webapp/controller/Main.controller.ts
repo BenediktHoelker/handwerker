2aa14aa6223adf9ef9a21455c8eaac62a71569c0
Benedikt Hölker <benedikt.hoelker@iot-online.de>
2023-03-19 15:03:57 +0100

    chore: wip prettier-config

---
+++ app/handwerker.testtypescript/webapp/controller/Main.controller.ts
@@ -0,0 +1,14 @@
+import MessageBox from "sap/m/MessageBox";
+import BaseController from "./BaseController";
+import formatter from "../model/formatter";
+
+/**
+ * @namespace handwerker.testtypescript.controller
+ */
+export default class Main extends BaseController {
+  private formatter = formatter;
+
+  public sayHello(): void {
+    MessageBox.show("Hello World!");
+  }
+}
