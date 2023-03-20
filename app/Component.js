1059ec87847621441b6846fc5b42c3cbc986ebeb
Benedikt Hölker <benedikt.hoelker@iot-online.de>
2023-03-20 21:47:46 +0100

    chore: clean-up config

---
+++ app/Component.js
@@ -0,0 +1,56 @@
+sap.ui.define(
+  ['sap/ui/Device', 'sap/ui/model/json/JSONModel', 'sap/ui/core/UIComponent'],
+  (Device, JSONModel, UIComponent) =>
+    UIComponent.extend('handwerker.Component', {
+      metadata: {
+        manifest: 'json'
+      },
+
+      async init(...args) {
+        UIComponent.prototype.init.apply(this, ...args);
+
+        const router = this.getRouter();
+
+        this.setModel(new JSONModel({}), 'session');
+
+        // create the views based on the url/hash
+        router.initialize();
+      },
+
+      /**
+       * The component is destroyed by UI5 automatically.
+       * In this method, the ErrorHandler is destroyed.
+       * @public
+       * @override
+       */
+      destroy(...args) {
+        // call the base component's destroy function
+        UIComponent.prototype.destroy.apply(this, args);
+      },
+
+      /**
+       * This method can be called to determine whether the sapUiSizeCompact or sapUiSizeCozy
+       * design mode class should be set, which influences the size appearance of some controls.
+       * @public
+       * @return {string} css class, either 'sapUiSizeCompact' or 'sapUiSizeCozy' - or an empty string if no css class should be set
+       */
+      getContentDensityClass() {
+        if (this._sContentDensityClass === undefined) {
+          // check whether FLP has already set the content density class; do nothing in this case
+          if (
+            document.body.classList.contains('sapUiSizeCozy') ||
+            document.body.classList.contains('sapUiSizeCompact')
+          ) {
+            this._sContentDensityClass = '';
+          } else if (!Device.support.touch) {
+            // apply "compact" mode if touch is not supported
+            this._sContentDensityClass = 'sapUiSizeCompact';
+          } else {
+            // "cozy" in case of touch support; default for most sap.m controls, but needed for desktop-first controls like sap.ui.table.Table
+            this._sContentDensityClass = 'sapUiSizeCozy';
+          }
+        }
+        return this._sContentDensityClass;
+      }
+    })
+);
--- dist/components/orderitems/controller/App.controller.ts
+++
@@ -1,6 +0,0 @@
-import BaseController from './BaseController';
-
-/**
- * @namespace handwerker.components.orderitems.controller
- */
-export default class App extends BaseController {}
