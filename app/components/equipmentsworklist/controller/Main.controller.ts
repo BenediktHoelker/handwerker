1746090468a1b624e446e326b79917d723b112c9
Benedikt Hölker <benedikt.hoelker@iot-online.de>
2023-04-13 22:29:33 +0200

    wip

--- app/components/equipmentsworklist/controller/Main.controller.ts
+++ app/components/equipmentsworklist/controller/Main.controller.ts
@@ -1,17 +1,13 @@
 import BaseController from './BaseController';
 import formatter from '../model/formatter';
-import List from 'sap/m/List';
-import SplitApp from 'sap/m/SplitApp';
 import UI5Event from 'sap/ui/base/Event';
 import ODataModel from 'sap/ui/model/odata/v2/ODataModel';
 import ODataListBinding from 'sap/ui/model/odata/v2/ODataListBinding';
 import Table from 'sap/m/Table';
-import ColumnListItem from 'sap/m/ColumnListItem';
 import Button from 'sap/m/Button';
 import MessageToast from 'sap/m/MessageToast';
 import ResourceBundle from 'sap/base/i18n/ResourceBundle';
 import MessageBox from 'sap/m/MessageBox';
-import ReuseComponentSupport from 'sap/suite/ui/generic/template/extensionAPI/ReuseComponentSupport';
 
 /**
  * @namespace handwerker.components.equipmentsworklist.controller
